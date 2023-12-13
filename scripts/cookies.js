6382f2769d3bf3f9a9256a4ae11546e7557e7abf
Christian <info.chr.alessandri@gmail.com>
2023-12-13 20:27:39 +0100

    Updated privacy and cookie policy

--- scripts/cookies.js
+++ scripts/cookies.js
@@ -3,6 +3,18 @@ var cookiesStatus = localStorage.getItem("cookies");
 var cookies = document.getElementById('cookies');
 var cookiesBtn = document.getElementById('cookies-btn');
 
+function acceptCookies() {
+  if (typeof gtag === 'function') {
+    gtag('config', 'G-BEP1R6E73X');
+  }
+
+  hideCookies();
+}
+
+function rejectCookies() {
+  hideCookies();
+}
+
 function hideCookies() {
   cookies.style.display = 'none';
   cookiesBtn.style.display = 'none';
